89af55c7ef0bb70191d82817157fdbd8901c3211
Philipp Zimmerer <ikepzimm@ike.uni-stuttgart.de>
2018-07-11 13:36:58 +0200

    Added support for decayed learning rate and l2 regularization.

--- main.py
+++ main.py
@@ -13,8 +13,15 @@ n_labels = 1
 layout = (350,350,350,350,350)
 actfunct = (tf.nn.sigmoid, tf.nn.sigmoid, tf.nn.sigmoid, tf.nn.sigmoid, tf.nn.sigmoid)
 optimization_algo = tf.train.AdamOptimizer
+beta = 0.005
 #optimization_algo = tf.train.GradientDescentOptimizer
-learning_rate = None
+#learning_rate = None
+learning_rate = 0.1
+decay_steps = 10000
+decay_rate = 0.95
+init_method = 5
+init_stddev = 0.5
+#batch_size=128
 
 #Define Parameters for the data
 #filename = 'waterset.xlsx'
@@ -42,10 +49,13 @@ testlabels = testset[label_indices].values
 
 #for i in range(4,6,1):
     #for j in range(100,500,50):
-randomize_dset = (True,)
-b_size_test = (32, 64, 128, 256, 512)
+randomize_dset = (True, False)
+b_size_test = (32, 64, 128, 256)
+#stddevtpl = (0.01, 0.1, 0.5, 1)
 
 for rand_dataset in randomize_dset:
+#for init_stddev in stddevtpl:
+#    for init_method in range(1,7,1):
     for batch_size in b_size_test:
         #layout = [] #jetzt dürfen es keine tuples mehr sein!
         #actfunct = []
@@ -56,9 +66,9 @@ for rand_dataset in randomize_dset:
         starttime = time.time()
 
         water_nn = neuralNet.neuralnet(n_features=n_features, n_labels=n_labels, layout=layout, actfunct=actfunct)
-        water_nn.build(optimization_algo=optimization_algo, learning_rate=learning_rate, init_method = 1, init_stddev = 1)
+        water_nn.build(optimization_algo=optimization_algo, learning_rate=learning_rate, beta=beta, init_method = init_method, init_stddev = init_stddev, decay_steps = decay_steps, decay_rate = decay_rate)
         water_nn.initializeSession()
-        water_nn.trainNP(trainfeatures=trainfeatures, trainlabels=trainlabels, max_epochs=3000, stop_error=None, batch_size=batch_size, RANDOMIZE_DATASET=rand_dataset, PLOTINTERACTIVE = False, STATS=True)
+        water_nn.trainNP(trainfeatures=trainfeatures, trainlabels=trainlabels, max_epochs=1500, stop_error=None, batch_size=batch_size, RANDOMIZE_DATASET=rand_dataset, PLOTINTERACTIVE = False, STATS=True)
 
         delta_t = time.time() - starttime
 
@@ -74,7 +84,7 @@ for rand_dataset in randomize_dset:
             configfile.write('learning rate: None\n')
             configfile.write('Time used for Training:' + str(delta_t) + '\n')
             configfile.write('Batch Size:' + str(batch_size) + '\n')
-            configfile.write('Randomize Dataset:' + str(rand_dataset) + '\n')
+            configfile.write('Randomize Dataset: ' + str(rand_dataset) + '\n')
 
         plt.plot(water_nn.lossprint[0], water_nn.lossprint[1])
         plt.title('Loss over Epochs')
@@ -95,6 +105,13 @@ for rand_dataset in randomize_dset:
         bulkspec_enthalpy = testset['Enthalpy local h_local (new)'].values
         walltemp_dns = (bulkspec_enthalpy, testset[label_indices].values)
         walltemp_dnn = (bulkspec_enthalpy, predictlabels)
+
+        predict_train = water_nn.predictNP(trainfeatures)
+        bulkspec_ent_train = trainset['Enthalpy local h_local (new)'].values
+        walltemp_train_dns = (bulkspec_ent_train, trainset[label_indices].values)
+        walltemp_train_dnn = (bulkspec_ent_train, predict_train)
+
+
 
         fig = plt.figure()
         ax = fig.add_subplot(1,1,1)
@@ -117,6 +134,30 @@ for rand_dataset in randomize_dset:
         plt.legend(loc=2)
         plt.savefig(fname=(directory + '/temp-dns-vs-dnn'))
         plt.gcf().clear()
+
+        fig5 = plt.figure()
+        ax5 = fig5.add_subplot(1,1,1)
+        ax5.scatter(walltemp_train_dns[0], walltemp_train_dns[1], alpha=0.8, c='red', edgecolors='black', marker='^', s=30, label='DNS')
+        ax5.scatter(walltemp_train_dnn[0], walltemp_train_dnn[1], alpha=0.8, c='blue', edgecolors='black', marker='o', s=30, label='DNN')
+        plt.title('Walltemp DNS vs DNN over bulk specific enthalpy in trainset')
+        plt.legend(loc=2)
+        plt.savefig(fname=(directory + '/temp-dns-dnn-over-hbulk-train'))
+        plt.gcf().clear()
+
+        fig6 = plt.figure()
+        ax6 = fig6.add_subplot(1,1,1)
+        ax6.scatter(walltemp_train_dnn[1], walltemp_train_dns[1], alpha=0.8, c='red', edgecolors='black', marker='^', s=30, label='noLBL')
+        pltmin = np.amin((np.amin(walltemp_train_dnn[1]),np.amin(walltemp_train_dns[1])))
+        pltmax = np.amax((np.amax(walltemp_train_dnn[1]),np.amax(walltemp_train_dns[1])))
+        plt.ylim(ymax = pltmax, ymin = pltmin)
+        plt.xlim(xmax = pltmax, xmin = pltmin)
+        plt.plot([pltmin, pltmax], [pltmin, pltmax], color='k', linestyle='-', linewidth=2)
+        plt.title('Walltemp DNS vs DNN in training set')
+        plt.legend(loc=2)
+        plt.savefig(fname=(directory + '/temp-dns-vs-dnn-train'))
+        plt.gcf().clear()
+
+        plt.close('all')
 
         water_nn.closeSession()
 
