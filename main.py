acc8bff7e17ea1b0e436cc25fe59632e95713cf2
pzimm-stud <39569688+pzimm-stud@users.noreply.github.com>
2018-08-15 14:18:54 +0200

    Restore tst (#3)
    * Added support for save and restore
    
    * Added support for early stopping based on best value and max_nonchanging_epochs.
    
    * Print AAD and MSE.

--- main.py
+++ main.py
@@ -56,10 +56,14 @@ op_point = sco2_data.drop(mask[~( mask['inlet_temp'] & mask['heat_flux'] & mask[
 sco2_data = sco2_data.drop(op_point.index)
 
 #Split the dataset into test train and validation set
-trainset = sco2_data.sample(frac=0.8)
-testset = sco2_data.drop(trainset.index)
-validset = trainset.sample(frac=0.2)
-trainset = trainset.drop(validset.index)
+#trainset = sco2_data.sample(frac=0.8)
+#testset = sco2_data.drop(trainset.index)
+#validset = trainset.sample(frac=0.2)
+#trainset = trainset.drop(validset.index)
+
+trainset = pd.read_excel('trainset.xlsx')
+validset = pd.read_excel('validset.xlsx')
+testset = pd.read_excel('testset.xlsx')
 
 #Preprocess the data (StandardScaling)
 mean = trainset[sco2_feature_indices].mean()
@@ -68,7 +72,7 @@ std = trainset[sco2_feature_indices].std()
 trainset[sco2_feature_indices] = (trainset[sco2_feature_indices] - mean )/  std
 testset[sco2_feature_indices] = (testset[sco2_feature_indices] - mean )/  std
 validset[sco2_feature_indices] = (validset[sco2_feature_indices] - mean )/  std
-op_point[sco2_feature_indices] = (op_point[sco2_feature_indices] - mean )/ std
+#op_point[sco2_feature_indices] = (op_point[sco2_feature_indices] - mean )/ std
 
 
 #Import and preprocess the data (as DataFrame):
@@ -95,170 +99,27 @@ water_nn.build(optimization_algo=optimization_algo, learning_rate=learning_rate,
 water_nn.initialize(init_method = init_method, init_stddev = init_stddev)
 water_nn.layeroperations()
 water_nn.initializeSession()
-water_nn.trainNP(trainfeatures=trainfeatures, trainlabels=trainlabels, max_epochs=5000, validfeatures = validfeatures , validlabels = validlabels, stop_error=None, batch_size=batch_size, RANDOMIZE_DATASET=True, PLOTINTERACTIVE = False, STATS=True)
+water_nn.trainNP(trainfeatures=trainfeatures, trainlabels=trainlabels, max_epochs=5000, validfeatures = validfeatures , validlabels = validlabels, stop_error=2, batch_size=batch_size, RANDOMIZE_DATASET=True, PLOTINTERACTIVE = False, STATS=True)
 
-delta_t = time.time() - starttime
+#water_nn.saveToDisk(path='./save/sco2')
 
 
-water_nn1 = neuralNet.neuralnet(n_features=n_features, n_labels=n_labels, layout=layout, actfunct=actfunct)
-water_nn1.build(optimization_algo=optimization_algo, learning_rate=learning_rate, beta=beta, decay_steps = decay_steps, decay_rate = decay_rate, BATCH_NORM = True, dropout_rate=0.4)
-water_nn1.initialize(init_method = init_method, init_stddev = init_stddev)
-water_nn1.layeroperations()
-water_nn1.initializeSession()
-water_nn1.trainNP(trainfeatures=trainfeatures, trainlabels=trainlabels, max_epochs=5000, validfeatures = validfeatures , validlabels = validlabels, stop_error=None, batch_size=batch_size, RANDOMIZE_DATASET=True, PLOTINTERACTIVE = False, STATS=True)
+#water_nn.restoreFromDisk(path='./save/sco2')
 
-print('MSE, AAD in testset, without dropout:')
+
+
+
+#print(op_point[sco2_feature_indices].values)
+#print(water_nn.predictNP(op_point[sco2_feature_indices].values))
+#print(water_nn.predictNPMSE(op_point[sco2_feature_indices].values, op_point[sco2_label_indices].values))
+
+#print(water_nn.predictNP(testfeatures))
+print('Validation Set:')
+print(water_nn.predictNPMSE(validfeatures, validlabels))
+print('Testset:')
 print(water_nn.predictNPMSE(testfeatures, testlabels))
-print('MSE, AAD at one op condition, without dropout:')
-print(water_nn.predictNPMSE(op_point[sco2_feature_indices].values, op_point[sco2_label_indices].values))
 
-print('MSE, AAD in testset, with dropout:')
-print(water_nn1.predictNPMSE(testfeatures, testlabels))
-print('MSE, AAD at one op condition, with dropout:')
-print(water_nn1.predictNPMSE(op_point[sco2_feature_indices].values, op_point[sco2_label_indices].values))
-
-
-plt.plot(water_nn.lossprint[0], water_nn.lossprint[1], label='no-dropout')
-plt.plot(water_nn.lossprint[0], water_nn1.lossprint[1], label='dropout')
-plt.title('Loss over Epochs')
-plt.yscale('log')
-plt.ylabel('Loss')
-plt.xlabel('Epoch')
-plt.legend(loc=1)
-plt.savefig(fname=('./loss-vs-time'))
-plt.gcf().clear()
-
-plt.plot(water_nn.aadprint[0], water_nn.aadprint[1], label='no-dropout')
-plt.plot(water_nn.aadprint[0], water_nn1.aadprint[1], label='dropout')
-plt.title('AAD over Epochs')
-plt.ylabel('AAD in %')
-plt.xlabel('Epoch')
-plt.legend(loc=1)
-plt.savefig(fname=('./aad-vs-time'))
-plt.gcf().clear()
-
-plt.plot(water_nn.learnprint[0], water_nn.learnprint[1])
-plt.title('Learning Rate over Epochs')
-plt.ylabel('Learning Rate')
-plt.xlabel('Epoch')
-plt.savefig(fname=('./lrate-vs-time'))
-plt.gcf().clear()
-
-plt.plot(water_nn.validlossprint[0], water_nn.validlossprint[1], label='no-dropout')
-plt.plot(water_nn.validlossprint[0], water_nn1.validlossprint[1], label='dropout')
-plt.title('Loss over Epochs in validset')
-plt.yscale('log')
-plt.ylabel('Loss')
-plt.xlabel('Epoch')
-plt.legend(loc=1)
-plt.savefig(fname=('./loss-vs-time-valid'))
-plt.gcf().clear()
-
-plt.plot(water_nn.validaadprint[0], water_nn.validaadprint[1], label='no-dropout')
-plt.plot(water_nn.validaadprint[0], water_nn1.validaadprint[1], label='dropout')
-plt.title('AAD over Epochs in validset')
-plt.ylim(ymax=10, ymin=0)
-plt.ylabel('AAD in %')
-plt.xlabel('Epoch')
-plt.legend(loc=1)
-plt.savefig(fname=('./aad-vs-time-valid'))
-plt.gcf().clear()
-
-
-
-sco2_predictlabels = water_nn.predictNP(testfeatures)
-sco2_bulkspec_enthalpy = testset[sco2_feature_indices[4]].values
-sco2_walltemp_dns = (sco2_bulkspec_enthalpy, testset[sco2_label_indices].values)
-sco2_walltemp_dnn = (sco2_bulkspec_enthalpy, sco2_predictlabels)
-
-
-predict_train = water_nn.predictNP(trainfeatures)
-bulkspec_ent_train = trainset[sco2_feature_indices[4]].values
-walltemp_train_dns = (bulkspec_ent_train, trainset[sco2_label_indices].values)
-walltemp_train_dnn = (bulkspec_ent_train, predict_train)
-
-sco2_predict_op = water_nn.predictNP(op_point[sco2_feature_indices].values)
-sco2_bulkspec_enthalpy_op =  op_point[sco2_feature_indices[4]].values
-sco2_walltemp_dns_op = ( sco2_bulkspec_enthalpy_op, op_point[sco2_label_indices].values )
-sco2_walltemp_dnn_op = ( sco2_bulkspec_enthalpy_op, sco2_predict_op)
-
-
-
-
-fig2 = plt.figure()
-ax2 = fig2.add_subplot(1,1,1)
-ax2.scatter(sco2_walltemp_dns[0], sco2_walltemp_dns[1], alpha=0.8, c='red', edgecolors='black', marker='^', s=30, label='DNS')
-ax2.scatter(sco2_walltemp_dnn[0], sco2_walltemp_dnn[1], alpha=0.8, c='blue', edgecolors='black', marker='o', s=30, label='DNN')
-plt.title('Walltemp DNS vs DNN over bulk specific enthalpy with sCO2')
-plt.legend(loc=2)
-plt.savefig(fname=('./temp-dns-dnn-over-hbulk-sco2'))
-plt.gcf().clear()
-
-
-
-fig5 = plt.figure()
-ax5 = fig5.add_subplot(1,1,1)
-ax5.scatter(sco2_walltemp_dnn[1], sco2_walltemp_dns[1], alpha=0.8, c='red', edgecolors='black', marker='^', s=30, label='noLBL')
-pltmin = np.amin((np.amin(sco2_walltemp_dnn[1]),np.amin(sco2_walltemp_dns[1])))
-pltmax = np.amax((np.amax(sco2_walltemp_dnn[1]),np.amax(sco2_walltemp_dns[1])))
-plt.ylim(ymax = pltmax, ymin = pltmin)
-plt.xlim(xmax = pltmax, xmin = pltmin)
-plt.plot([pltmin, pltmax], [pltmin, pltmax], color='k', linestyle='-', linewidth=2)
-plt.title('Walltemp DNS vs DNN in validation set with sCO2')
-plt.legend(loc=2)
-plt.savefig(fname=('./temp-dns-vs-dnn-sCO2'))
-plt.gcf().clear()
-
-
-fig3 = plt.figure()
-ax3 = fig3.add_subplot(1,1,1)
-ax3.scatter(walltemp_train_dns[0], walltemp_train_dns[1], alpha=0.8, c='red', edgecolors='black', marker='^', s=30, label='DNS')
-ax3.scatter(walltemp_train_dnn[0], walltemp_train_dnn[1], alpha=0.8, c='blue', edgecolors='black', marker='o', s=30, label='DNN')
-plt.title('Walltemp DNS vs DNN over bulk specific enthalpy with sCO2 in trainset')
-plt.legend(loc=2)
-plt.savefig(fname=('./temp-dns-dnn-over-hbulk-sco2-train'))
-plt.gcf().clear()
-
-
-
-fig8 = plt.figure()
-ax8 = fig8.add_subplot(1,1,1)
-ax8.scatter(walltemp_train_dnn[1], walltemp_train_dns[1], alpha=0.8, c='red', edgecolors='black', marker='^', s=30, label='noLBL')
-pltmin = np.amin((np.amin(walltemp_train_dnn[1]),np.amin(walltemp_train_dns[1])))
-pltmax = np.amax((np.amax(walltemp_train_dnn[1]),np.amax(walltemp_train_dns[1])))
-plt.ylim(ymax = pltmax, ymin = pltmin)
-plt.xlim(xmax = pltmax, xmin = pltmin)
-plt.plot([pltmin, pltmax], [pltmin, pltmax], color='k', linestyle='-', linewidth=2)
-plt.title('Walltemp DNS vs DNN in training set with sCO2')
-plt.legend(loc=2)
-plt.savefig(fname=('./temp-dns-vs-dnn-sCO2-train'))
-plt.gcf().clear()
-
-fig9 = plt.figure()
-ax9 = fig9.add_subplot(1,1,1)
-ax9.scatter(sco2_walltemp_dns_op[0], sco2_walltemp_dns_op[1], alpha=0.8, c='red', edgecolors='black', marker='^', s=30, label='DNS')
-ax9.scatter(sco2_walltemp_dnn_op[0], sco2_walltemp_dnn_op[1], alpha=0.8, c='blue', edgecolors='black', marker='o', s=30, label='DNN')
-plt.title('Walltemp DNS vs DNN over bulk specific enthalpy with sCO2 one op condition')
-plt.legend(loc=2)
-plt.savefig(fname=('./temp-dns-dnn-over-hbulk-sco2-op'))
-plt.gcf().clear()
-
-fig4 = plt.figure()
-ax4 = fig4.add_subplot(1,1,1)
-ax4.scatter(sco2_walltemp_dnn_op[1], sco2_walltemp_dns_op[1], alpha=0.8, c='red', edgecolors='black', marker='^', s=30, label='noLBL')
-pltmin = np.amin((np.amin(sco2_walltemp_dnn_op[1]),np.amin(sco2_walltemp_dns_op[1])))
-pltmax = np.amax((np.amax(sco2_walltemp_dnn_op[1]),np.amax(sco2_walltemp_dns_op[1])))
-plt.ylim(ymax = pltmax, ymin = pltmin)
-plt.xlim(xmax = pltmax, xmin = pltmin)
-plt.plot([pltmin, pltmax], [pltmin, pltmax], color='k', linestyle='-', linewidth=2)
-plt.title('Walltemp DNS vs DNN at one op condition with sCO2')
-plt.legend(loc=2)
-plt.savefig(fname=('./temp-dns-vs-dnn-sCO2-op'))
-plt.gcf().clear()
-
-
-
-plt.close('all')
+#water_nn.saveToDisk(path='./savetest/water-nn-restored')
 
 #water_nn.saveToDisk((directory + '/tf-save'))
 water_nn.closeSession()
